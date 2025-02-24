cbb6b91e424c2e3021ed6f9ce86700ac34bad9fa
Synaptic2 <synaptiker@gmail.com>
2025-02-24 21:16:34 +0100

    Aktualisierung meiner Website

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector(".zx-button");
+    button.addEventListener("click", function() {
+        alert("Willkommen in der ZX Spectrum Welt!");
+    });
+});
