9bd4138972b97175cdcc6294b0a7f940c2e9a0b2
Synaptic2 <synaptiker@gmail.com>
2025-04-29 07:45:38 +0200

    Delete gabel1.js

--- gabel1.js
+++
@@ -1,5 +0,0 @@
-function growFork() {
-    let handle = document.getElementById("handle");
-    let currentHeight = handle.style.height ? parseInt(handle.style.height) : 50;
-    handle.style.height = (currentHeight + 20) + "px";
-}
